def8c5a6dedf6512a2ece76afde5f3993acf41b2
fardi1312 <massoud.fardi@esprit.tn>
2023-11-13 13:50:59 +0100

    commit change un entity et twig

--- src/Controller/Covoiturage/CovoiturageController.php
+++ src/Controller/Covoiturage/CovoiturageController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('')]
+#[Route('/covoiturage')]
 class CovoiturageController extends AbstractController
 {
     #[Route('/', name: 'app_covoiturage_index', methods: ['GET'])]
