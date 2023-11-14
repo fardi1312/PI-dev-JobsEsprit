57c7b0d25016ef8140fbfbab07c43ccc050f558a
your_github_name <benothmen.ons@esprit.tn>
2023-11-14 21:55:19 +0100

    dashbord admin Event table

--- src/Controller/DashbordController.php
+++ src/Controller/DashbordController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Calendaractivity;
+use App\Repository\CalendaractivityRepository;
 use App\Repository\OffreRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,6 +28,14 @@ class DashbordController extends AbstractController
             'offres' => $offrerepository->findAll(),
         ]);
     }
+    #[Route('/adminevents', name: 'events_admin')]
+    public function listEvent(CalendaractivityRepository $offrerepository): Response
+    {
+
+        return $this->render('dashboard/interview.html.twig', [
+            'calendaractivities' => $offrerepository->findAll(),
+        ]);
+    }
     #[Route('/book/delete/{id}', name: 'admin_deleteoffre')]
     public function deleteBook(Request $request, $id, ManagerRegistry $manager, OffreRepository $offreRepository): Response
     {
@@ -36,5 +46,16 @@ class DashbordController extends AbstractController
         $em->flush();
     
         return $this->redirectToRoute('list_offreadmin');
+    }
+    #[Route('/event/delete/{id}', name: 'admin_deleteeevnt')]
+    public function deleteEvent(Request $request, $id, ManagerRegistry $manager, CalendaractivityRepository $offreRepository): Response
+    {
+        $em = $manager->getManager();
+        $book = $offreRepository->find($id);
+    
+        $em->remove($book);
+        $em->flush();
+    
+        return $this->redirectToRoute('events_admin');
     }
 }
