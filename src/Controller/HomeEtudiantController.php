9db29bbcfe6fcba6a0d7e7193cef5d7c5110fa8c
fardi1312 <massoud.fardi@esprit.tn>
2023-11-17 00:41:18 +0100

    12 commit

--- src/Controller/HomeEtudiantController.php
+++ src/Controller/HomeEtudiantController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface; // Assurez-vous d'avoir cette ligne ajoutée
 
 class HomeEtudiantController extends AbstractController
 {
     #[Route('/home/etudiant', name: 'app_home_etudiant')]
-    public function index(): Response
+    public function index(SessionInterface $session): Response
     {
+        // Récupérer les données de l'utilisateur depuis la session
+        $user = $session->get('user');
         return $this->render('baseEtudiant.html.twig', [
-            'controller_name' => 'HomeEtudiantController',
+            'user' => $user,
         ]);
     }
+
+
+
+
 }
